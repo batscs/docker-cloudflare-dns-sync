bbe035d9046eb9e46a47b98f689659256cad73d3
bats <31670615+batscs@users.noreply.github.com>
2024-01-30 23:14:51 +0100

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -2,7 +2,7 @@
 # A Script to automatically deploy a docker container with the cloudflare-dns-sync script running
 # No Cronjobs are set by default
 echo "Docker: Building Image batscs/cloudflage-dns-sync"
-docker build --no-cache -t batscs/cloudflare-dns-sync:latest https://raw.githubusercontent.com/batscs/cloudflare-dns-sync/main/Dockerfile
+docker build --no-cache -t batscs/cloudflare-dns-sync:latest https://raw.githubusercontent.com/batscs/docker-cloudflare-dns-sync/main/Dockerfile
 
 echo "Docker: Creating Container cf-sync with Image batscs/cloudflare-dns-sync"
 docker run -dit --name cf-sync --restart unless-stopped batscs/cloudflare-dns-sync
